6b30925321e7e8d580ff78e7a29422520834df61
Ozgun Ataman <ozataman@gmail.com>
2013-04-13 17:02:27 -0400

    Add WordCount example

---
+++ test/WordCount.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE TupleSections             #-}
+
+module Main where
+
+-------------------------------------------------------------------------------
+import qualified Data.ByteString.Char8 as B
+import           Data.Conduit
+import qualified Data.Conduit.List     as C
+import           Data.CSV.Conduit
+import           Data.Default
+import           Hadoop.Streaming
+-------------------------------------------------------------------------------
+
+
+main :: IO ()
+main = mapReduceMain mro mapper' reducer' (fromCSV def)
+
+
+mro = MROptions (==) 1 pSerialize
+
+
+mapper' = linesConduit =$= C.concatMap f
+    where
+      f ln = map (\w -> ([w], 1)) $ B.words ln
+
+
+reducer' :: Monad m => Reducer Int m (Row B.ByteString)
+reducer' = do
+  (w, cnt) <- C.fold (\ (_, cnt) ([k], x) -> (k, cnt + x)) ("", 0)
+  yield $ [w, B.pack . show $ cnt]
+
+
+
